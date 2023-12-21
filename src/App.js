1bfebbd8381b2176804e4c6aaa36254e9d307358
tatianasnook <tatsnook@gmail.com>
2023-12-21 08:30:45 -0600

    useref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import video from './clouds.mp4';
 
 
 function App() {
+  const text = useRef();
   const [activity, setActivity] = useState('');
 
-  const changeFont = () => {
-    const h3Element = document.getElementsByTagName('h3')[0];
-    h3Element.classList.add('change');
-  }
-  
   useEffect(() => {
     getActivity();
   },  [])
 
   const getActivity = async () => {
+    text.current.classList.remove('change')
       const response = await fetch('http://www.boredapi.com/api/activity/');
       const data = await response.json();
       setActivity(data.activity);
-      changeFont();
+      text.current.classList.add('change')
   }
 
   return (
@@ -30,7 +27,7 @@ function App() {
         </video>
         <h1>Are you bored?</h1>
         <h2>100+ Things to Do When You're Bored to Pass the Time</h2>
-        <h3>{activity}</h3>
+        <h3 ref={text}>{activity}</h3>
       </div>
       <button onClick={getActivity}>Press here to get an idea</button>
     </div>
