0da19ab37fdad3a475f38a690a785da1ad9b2962
tatianasnook <tatsnook@gmail.com>
2023-12-21 14:22:06 -0600

    change to video

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-        <video autoPlay muted loop>
+        <video autoPlay muted loop playsInline>
           <source src={video} type="video/mp4"/>
         </video>
         <h1>Are you bored?</h1>
